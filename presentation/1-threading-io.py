6036bc79f1afd3ad8e03376b77788ca345d2d274
Adenegar <adenegar9302@gmail.com>
2025-04-06 13:41:16 -0600

    removed unnecessary code

--- presentation/1-threading-io.py
+++ presentation/1-threading-io.py
@@ -1,5 +1,4 @@
 import threading
-import time
 
 def count():
     print("One")
@@ -8,16 +7,15 @@ def count():
 
 def main():
     threads = []
+    # Start threads
     for _ in range(3):
         thread = threading.Thread(target=count)
         threads.append(thread)
         thread.start()
     
+    # Stop threads
     for thread in threads:
         thread.join()
 
 if __name__ == "__main__":
-    start_time = time.perf_counter()
     main()
-    elapsed = time.perf_counter() - start_time
-    print(f"{__file__} executed in {elapsed:0.2f} seconds.")
